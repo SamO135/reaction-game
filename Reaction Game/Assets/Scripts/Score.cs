4f99294138b6a35a26295da1cf782cdeedd662f3
Sam <samuel.ogg01@gmail.com>
2022-06-14 15:09:15 +0100

    Score decimals
    - Added the option to include decimals in the score as you could often end up with the same score when just using integers.
    
    - The number of decimals can be changed from the inspector on the 'Score' script/component.

--- Reaction Game/Assets/Scripts/Score.cs
+++ Reaction Game/Assets/Scripts/Score.cs
@@ -9,15 +9,19 @@ public class Score : MonoBehaviour
     public Transform player;
     public TMP_Text scoreText;
     [HideInInspector]
-    public float score;
+    public string score;
     private RectTransform scoreTransform;
     //public GameController gameController;
     public GameObject endGameScreen;
+    public int decimals;
+    private float startingPos;
+
     // Start is called before the first frame update
     void Awake()
     {
         scoreText = GetComponent<TMP_Text>();
-        score = 0;
+        score = "0";
+        startingPos = player.position.y;
 
         //endGameScreen = GameObject.Find("EndGame");
     }
@@ -30,7 +34,10 @@ public class Score : MonoBehaviour
         else
             scoreText.enabled = true;
 
-        score = Mathf.Round(player.position.y + 3);
-        scoreText.text = score.ToString();
+        score = (player.position.y - startingPos).ToString("F" + decimals.ToString());
+
+        if (float.Parse(score) <= 0.0f)
+            score = "0";
+        scoreText.text = score;
     }
 }
